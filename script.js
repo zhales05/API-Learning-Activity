7ba2d8d9e084c87f2f6b7eec88e6fff2dc626b14
zhales05 <59749864+zhales05@users.noreply.github.com>
2020-11-03 14:56:04 -0700

    Implemented
    Got Fetch to work

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("emailSubmit").addEventListener("click", function(event) {
+    event.preventDefault();
+    const value = document.getElementById("emailInput").value;
+    if (value === "")
+      return;
+    console.log(value); 
+
+   
+
+     const url = "https://api.agify.io?name=" + value;
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        let results = "";
+        results ="<h2>Results</h2>";
+        results += "<p> Your age should be: ";
+        results += json.age;
+        results += "</p>";
+       document.getElementById("output").innerHTML = results;
+       document.getElementById("output").style.border = "thick solid #0000FF";
+      });
+});
